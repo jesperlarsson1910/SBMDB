587884405c637865e7172b0ecf1371f112476284
Jesper Larsson <jesper.larsson@iths.se>
2026-03-09 16:03:57 +0100

    initial commit
    Signed-off-by: Jesper Larsson <jesper.larsson@iths.se>

---
+++ src/main/java/org/example/sbmdb/SbmdbApplication.java
@@ -0,0 +1,13 @@
+package org.example.sbmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbmdbApplication.class, args);
+    }
+
+}
